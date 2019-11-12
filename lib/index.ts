b19ec93869e7b7a9ddeeb524fc55e2b9f714b295
Ender Bonnet <enbonnet@pm.me>
2019-11-12 17:51:25 -0300

    feat(scrollmove): add function

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,16 @@
-export default function() {}
+/**
+ * Move scroll smooth to center element id selected.
+ *
+ * @param  {string} id - Id of element selected.
+ * @example
+ * scrollMode(#id)
+ * @returns {null} Void.
+ */
+const scrollMove = (id: string) => {
+  const element = document.querySelector(id) || null;
+  if (element) {
+    element.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
+};
+
+export default scrollMove;
